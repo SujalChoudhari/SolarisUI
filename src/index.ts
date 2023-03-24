f5d12ff1c8615a587f33e98b0d18dc64776bc252
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-03-24 08:36:34 +0530

    🎨 Code Updated for Alpha Release

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
+/**
+ * @packageDocumentation
+ * 
+ * SolarisUI is a UI component-based framework that allows you to easily create static 
+ * websites without worrying about writing your own HTML or CSS files.
+ * 
+ * ## Index.ts
+ * The `Index.ts` file imports and 
+ * re-exports all the underlying classes.
+ * 
+ * ## SolarisUI class
+ * The `SolarisUI` class provides the main functionality of the framework. 
+ * It allows you to create and manage UI components for your static website.
+ * 
+ * @license MIT license
+ */
+
+
 import {
     Body,
     Button,
@@ -176,6 +194,7 @@ class SolarisUI {
     }
 }
 
+export default SolarisUI;
 
 export {
     SolarisUI,
