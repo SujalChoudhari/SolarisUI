1e028c2e7124cb91b302ed228c27ebaf43b15900
Sujal Choudhari <sjlchoudhari@gmail.com>
2023-03-29 09:30:17 +0530

    🔊 Save Logs
    Logs are now saved under
    ./logs-day-month-year-hr-min.log

--- src/logger.ts
+++ src/logger.ts
@@ -1,4 +1,5 @@
 import FileManager from "./filemanager";
+import fs from 'fs';
 export enum LogLevel {
     DEBUG,
     INFO,
@@ -8,6 +9,7 @@ export enum LogLevel {
 
 export default class Logger {
     public static logLevel: LogLevel = LogLevel.INFO;
+    // private static fm = new FileManager();
     public static debug(filename: string, ...message: string[]): void {
         if (Logger.logLevel <= 0)
             Logger.write("[DEBUG]", filename, ...message);
@@ -30,6 +32,17 @@ export default class Logger {
 
 
     private static write(type: string, filename: string, ...message: string[]): void {
-        console.log(type + `: (${filename})\n\t` + message.join(" "));
+        // console.log(type + `: (${filename})\n\t` + message.join(" "));
+
+        if (!fs.existsSync('./logs'))
+            fs.mkdirSync("./logs")
+
+        const date = new Date();
+        const filePath = `./logs/${date.getFullYear()}-${date.getMonth()}-${date.getDate()}-${date.getHours()}-${date.getMinutes()}.log`;
+
+        if (!fs.existsSync(filePath))
+            fs.writeFileSync(filePath,`${type} :(${filename})\t ${message.join(" ")}\n`);
+        else
+            fs.appendFileSync(filePath,`${type}:\t(${filename})\t ${message.join(" ")}\n`);
     }
 }
