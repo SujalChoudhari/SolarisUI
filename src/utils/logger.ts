75f369a64ffbb96aeda080b661fddd50e718ea73
Ansh Sharma Games and Technology <73456520+Ansh-code398@users.noreply.github.com>
2023-04-12 19:23:44 +0530

    Fixed  things which I broke in last commit

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,4 @@
-import FileManager from "./filemanager";
 import fs from 'fs';
-import { red, yellow, green, blue, cyan, gray, bold, italic, redBright, yellowBright, cyanBright, blueBright, greenBright } from 'ansi-colors';
 export enum LogLevel {
     DEBUG,
     INFO,
